8fc5b9882e2bd87bbe6b3829d87bbeace762847b
Raúl Aguilar Arroyo <100472050@alumnos.uc3m.es>
2023-09-09 11:50:26 +0200

    final

--- tester.py
+++ tester.py
@@ -3,6 +3,10 @@ from src.cv import PlateDetector
 pd = PlateDetector()
 image_path = "test_images/N1.jpeg"
 
-for i in range(1, 33):
+for i in range(1, 248):
     image_path = f"test_images/N{i}.jpeg"
-    pd.detect_and_show(image_path)
+    try:
+        pd.detect_and_show(image_path)
+    except:
+        print(f"Could not read image {image_path}")
+        continue
