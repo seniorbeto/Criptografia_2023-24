568b94ef62d84161ff9bbeed75d2eb6d65d46e7c
Raúl Aguilar Arroyo <100472050@alumnos.uc3m.es>
2023-09-28 21:25:50 +0200

    he dejao amedias el trest del api

--- api.test.py
+++ api.test.py
@@ -1,26 +1,45 @@
-from src.api import ServerAPI
+import unittest
+from api import ServerAPI
 import datetime
+from freezegun import freeze_time
 
-api = ServerAPI()
-api.server.clear_server()
+class TestServerAPI(unittest.TestCase):
+    def setUp(self):
+        self.api = ServerAPI()
+        self.api.server.clear_server()
+        self.api.register("user1", "pass1")
+        self.api.register("user2", "pass2")
+        
+    def test_upload_and_get_images(self):
+        self.api.login("user1", "pass1")
+        self.api.upload_photo("test.png")
+        self.api.logout()
+        
+        self.api.login("user2", "pass2")
+        self.api.upload_photo("test.png")
+        self.api.logout()
+        
+        self.api.login("user1", "pass1")
+        images = self.api.get_images(10)
+        self.assertEqual(len(images), 2)
+        self.api.logout()
+        
+    def test_get_images_by_date(self):
+        self.api.login("user1", "pass1")
+        self.api.upload_photo("test.png")
+        self.api.logout()
+        
+        self.api.login("user1", "pass1")
+        now = datetime.datetime.now()
+        images = self.api.get_images(10, date=now.strftime("%Y/%m/%d"))
+        self.assertEqual(len(images), 1)
+        self.api.logout()
+    
+    @freeze_time("2022-01-01")
+    def test_upload_and_get_images(self):
+        self.api.login("user1", "pass1")
+        
+        # get images from 
 
-api.register("user1", "pass1")
-api.register("user2", "pass2")
-
-api.login("user1", "pass1")
-api.upload_photo("test.png")
-
-api.login("user2", "pass2")
-api.upload_photo("test.png")
-
-api.logout()
-
-print("sacando imagenes genericas")
-print(api.get_images(10))
-print("     De user1: ", api.get_images(10))
-
-print("logeando a user1")
-api.login("user1", "pass1")
-now = datetime.datetime.now()
-print("     Del dia de hoy del usuario", api.get_images(10, date=now.strftime("%Y/%m/%d")))
-
+if __name__ == '__main__':
+    unittest.main()
