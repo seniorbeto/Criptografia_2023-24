cd23934dd95c7c40f6ee531f44313167a2e69d13
Beto <100471939@alumnos.uc3m.es>
2023-09-29 10:43:08 +0200

    Mejora en el loggin y visualización de imágenes

--- src/screens/user_screen.py
+++ src/screens/user_screen.py
@@ -15,10 +15,12 @@ class UserScreen(tk.Frame):
 
         self.display_main_menu()
 
+        self.app.root.title(f"Logged as: {self.app.api.username}")
+
         # We create a canvas to put the scrollbar in it
         # Calculate de height that the menu is taking
         self.canvas = tk.Canvas(self, background="#212121", highlightthickness=0, borderwidth=0)
-        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=10, pady=10)
         self.scrollbar = tk.Scrollbar(self, orient=tk.VERTICAL, command=self.canvas.yview)
         self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
@@ -54,6 +56,7 @@ class UserScreen(tk.Frame):
         # We create the file menu
         self.file_menu = tk.Menu(self.main_menu, tearoff=0)
         self.main_menu.add_cascade(label="File", menu=self.file_menu)
+        self.file_menu.add_command(label="Add Image", command=self.add_image)
         self.file_menu.add_command(label="Exit", command=self.app.root.quit)
 
         # We create the user menu
@@ -68,8 +71,13 @@ class UserScreen(tk.Frame):
 
     def show_images(self):
         self.images = self.app.api.get_images(date="2022") # Date is temporal until issue #15 is solved
+        y = 0
         for i in range(len(self.images)):
             image = ImageTk.PhotoImage(self.images[i].resize((200, 200)))
             self.cache_images.append(image)
             image_label = tk.Label(self.canvas, image=image)
-            self.canvas.create_window((0, i * 70), window=image_label, anchor="nw")
+            if i%3 == 0:
+                y += 200
+            self.canvas.create_window(((i%3)*200, y), window=image_label, anchor="nw")
+
+    def add_image(self):...
