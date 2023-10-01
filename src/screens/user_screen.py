6efd4661fb63a542ce48007df1d57a0b6578fb5f
Beto <100471939@alumnos.uc3m.es>
2023-10-01 12:22:15 +0200

    Trabajando en el issue #20

--- src/screens/user_screen.py
+++ src/screens/user_screen.py
@@ -49,6 +49,10 @@ class UserScreen(tk.Frame):
     def on_mousewheel(self, event):
         self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
+    def show_context_menu(self, event):
+        context_menu = tk.Menu(self, tearoff=0)
+        context_menu.add_command(label="Delete", command=self.delete_image)
+        context_menu.post(event.x_root, event.y_root)
 
     def display_main_menu(self):
         self.main_menu = tk.Menu(self.app.root)
@@ -77,6 +81,7 @@ class UserScreen(tk.Frame):
             image = ImageTk.PhotoImage(self.images[i].resize((200, 200)))
             self.cache_images.append(image)
             image_label = tk.Label(self.canvas, image=image)
+            image_label.bind("<Button-3>", self.show_context_menu)
             if i%3 == 0:
                 y += 200
             self.canvas.create_window(((i%3)*200, y), window=image_label, anchor="nw")
@@ -86,3 +91,5 @@ class UserScreen(tk.Frame):
         if filepath:
             self.app.api.upload_photo(filepath)
             self.initiate_main_display() # Refresh
+
+    def delete_image(self):...
